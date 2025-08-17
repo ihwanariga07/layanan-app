5e906558db2dc9fa9a808ab5bba402d5dc66677b
Ihwan Ariga <ihwanariga.ia@gmail.com>
2025-08-17 21:52:24 +0700

    membuat guest dan menyesuaikan sidebar dan headbar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -65,10 +65,11 @@
                             <!-- <li><a class="lan-4" href="{{ url('admin') }}">Admin</a></li>
                             <li><a class="lan-5" href="{{ url('dashboard/layanan') }}">Layanan</a></li> -->
                             <li><a href="{{ route('admin.dashboard') }}">Beranda</a></li>
-                            <li><a href="{{ route('layanan.index') }}">Layanan</a></li>
-                            <li><a href="{{ route('customer.index') }}">Customer</a></li>
-                            <li><a href="{{ route('booking.index') }}">Booking</a></li>
-                            <li><a href="{{ route('booking_detail.index') }}">Booking Detail</a></li>
+                            <li><a href="{{ route('layanan.index') }}">Users</a></li>
+                            <li><a href="{{ route('customer.index') }}">layanan</a></li>
+                            <li><a href="{{ route('booking.index') }}">antrian</a></li>
+                            <li><a href="{{ route('booking_detail.index') }}">loket</a></li>
+                            <li><a href="{{ route('booking.index') }}">riwayat_panggilan</a></li>
                             <!-- Tambahkan menu dashboard lainnya di sini -->
                         </ul>
                     </li>
