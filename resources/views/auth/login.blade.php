e9e647c5ffb310c1e37f355de2065766c09cccea
Ihwan Ariga <ihwanariga.ia@gmail.com>
2025-08-18 16:46:58 +0700

    menghapus kembali template yang berhubungan dengan app.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.guest')
+@extends('layouts.app')
 
 @section('content')
 <div class="container">
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::redirect('/', '/login');
+Route::get('/', function () {
+    return view('welcome');
+})->middleware('auth');
+
 Auth::routes();
-
-Route::middleware('auth')->group(function (){
-
-        Route::middleware(['auth'])->group(function () {
-    Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
-});
-
-});
